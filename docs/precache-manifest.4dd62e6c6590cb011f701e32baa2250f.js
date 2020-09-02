764aa4c8073a2baad62b131d27c1f85265fdbb64
Yorest <yorest7@gmail.com>
2020-09-01 22:46:08 -0600

    Docs Agragado

---
+++ docs/precache-manifest.4dd62e6c6590cb011f701e32baa2250f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513cfe49b582e74cead7e1cc0eae0843",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a6234d4d9d5dbcd694b",
+    "url": "/static/css/main.8e0e1c75.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6234d4d9d5dbcd694b",
+    "url": "/static/js/main.e4466a16.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
